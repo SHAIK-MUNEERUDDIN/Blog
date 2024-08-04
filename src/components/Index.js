f66d384ebfbba2b7404204956cca1cff7daf2593
shaik-muneeruddin <skmuneeruddin53@gmail.com>
2024-08-04 23:32:38 +0530

    added new components

--- src/components/Index.js
+++ src/components/Index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, Input, LogoutBtn, Button };
